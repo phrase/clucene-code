5db21a79567342c8554a9c2574b773b7d824814b
Ben van Klinken <ustramooner@users.sourceforge.net>
2009-07-14 16:24:01 +0100

    these seem like really bad problems!
    Conflicts:
    
    	src/core/CLucene/search/ConjunctionScorer.cpp

--- src/core/CLucene/search/ConjunctionScorer.cpp
+++ src/core/CLucene/search/ConjunctionScorer.cpp
@@ -22,7 +22,7 @@ CL_NS_DEF(search)
 		coord(0.0),
 		lastDoc(-1)
 	{
-    this->scorers = _CLNEW CL_NS(util)::ObjectArray<Scorer>(_scorers->size());
+    this->scorers = _CLNEW CL_NS(util)::ValueArray<Scorer*>(_scorers->size());
     _scorers->toArray(this->scorers->values, false);
     coord = getSimilarity()->coord(this->scorers->length, this->scorers->length);
   }
@@ -33,7 +33,7 @@ CL_NS_DEF(search)
 		coord(0.0),
 		lastDoc(-1)
 	{
-    this->scorers = _CLNEW CL_NS(util)::ObjectArray<Scorer>(_scorers->length);
+    this->scorers = _CLNEW CL_NS(util)::ValueArray<Scorer*>(_scorers->length);
     memcpy(this->scorers->values, _scorers->values, _scorers->length * sizeof(Scorer*));
     coord = getSimilarity()->coord(this->scorers->length, this->scorers->length);
   }
@@ -86,7 +86,8 @@ CL_NS_DEF(search)
   bool ConjunctionScorer::init(int32_t target)  {
     firstTime = false;
     more = scorers->length>1;
-    
+
+
     for (size_t i=0; i<scorers->length; i++) {
       more = target==0 ? scorers->values[i]->next() : scorers->values[i]->skipTo(target);
       if (!more)
