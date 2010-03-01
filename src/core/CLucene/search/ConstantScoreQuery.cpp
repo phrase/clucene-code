7690ac65777a37369b6455df2ef1dff801666aee
Ben van Klinken <ustramooner@users.sourceforge.net>
2010-03-01 12:17:47 +0100

    fixes found by valgrind

--- src/core/CLucene/search/ConstantScoreQuery.cpp
+++ src/core/CLucene/search/ConstantScoreQuery.cpp
@@ -112,7 +112,7 @@ public:
             
             TCHAR* tmp = parentQuery->filter->toString();
             buf.append(tmp);
-            _CLLDELETE(tmp);
+            _CLDELETE_LCARRAY(tmp);
 
             buf.append(_T("), product of:"));
 
@@ -127,7 +127,7 @@ public:
             
             TCHAR* tmp = parentQuery->filter->toString();
             buf.append(tmp);
-            _CLLDELETE(tmp);
+            _CLDELETE_LCARRAY(tmp);
 
             buf.append(_T(") doesn't match id "));
             buf.appendInt(doc);
