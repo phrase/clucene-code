6af70ec90fa46e46a2b7d69a310556883ba54f8c
Veit Jahns <idolum@users.sourceforge.net>
2011-10-09 13:01:14 +0200

    Adding missing copyright notice (provided by Celto)

--- src/core/CLucene/index/IndexFileDeleter.cpp
+++ src/core/CLucene/index/IndexFileDeleter.cpp
@@ -1,3 +1,10 @@
+/*------------------------------------------------------------------------------
+ * Copyright (C) 2003-2006 Ben van Klinken and the CLucene Team
+ *
+ * Distributable under the terms of either the Apache License (Version 2.0) or
+ * the GNU Lesser General Public License, as specified in the COPYING file.
+ ------------------------------------------------------------------------------*/
+
 #if _MSC_VER == 1200
 #   pragma warning (disable : 4786)
 #endif
